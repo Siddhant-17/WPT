0e00c4447a1de520856dc98669eb855d65834c1d
Siddhant Shatrughna Jodmote <131379436+Siddhant-17@users.noreply.github.com>
2023-05-07 23:16:58 +0530

    Add files via upload

---
+++ WPT Assignment/7-march/function.js
@@ -0,0 +1,15 @@
+// function addition(a,b)
+// {
+//     console.log(a+b)
+// }
+
+// addition(4,5)               ---function call
+
+
+// function sub(a,b)           --function is keyword
+// {
+//     return a-b
+// }
+
+// let c=sub(5,4)
+//console.log(c)
